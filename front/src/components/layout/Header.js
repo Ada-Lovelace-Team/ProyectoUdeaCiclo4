44af5d7da5768f393c82a494559a3cc777940e0c
Daniel Tejada <dtejadah@unal.edu.co>
2022-10-27 07:45:07 -0500

    Componente para una barra de navegación navbar

--- front/src/components/layout/Header.js
+++ front/src/components/layout/Header.js
@@ -41,7 +41,5 @@ const Header = () => {
   )
 }
 
-
-
 export default Header
 
---
+++ front/src/components/layout/Navbar.js
@@ -0,0 +1,41 @@
+import React, { Fragment } from "react";
+
+export const Navbar = () => {
+  return (
+    <Fragment>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">
+                Home <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Mujeres
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Hombres
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" href="#">
+                Niños
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" href="#">
+                Promociones
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </Fragment>
+  );
+};
+
+export default Navbar;
